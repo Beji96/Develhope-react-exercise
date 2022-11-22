0035ea967dda19cee2767c896c20decfd910ccf0
Flavius Bejan <flaviusbejan96@yahoo.com>
2022-11-22 14:29:01 +0200

    finish cond2

--- src/Age.jsx
+++ src/Age.jsx
@@ -4,7 +4,7 @@ export class Age extends Component {
   render() {
     return (
       <div>
-        <p>My age is {this.props.age}</p>
+        {this.props.age && <p>My age is {this.props.age}</p>}
       </div>
     )
   }
