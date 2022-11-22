2a3cf61b34eb5c844cbe0fe6bee429f8b9eb4773
Flavius Bejan <flaviusbejan96@yahoo.com>
2022-11-22 10:37:11 +0200

    finish prop-3

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
  return(
   <div className="App">
     <Hello/>
-    <Welcome name ="Flavius"/>
-    <Welcome/>
+    <Welcome name ="Flavius" age ={26}/>
+    
   </div>
  )
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -9,7 +9,10 @@ import React, { Component } from 'react'
  class Welcome extends Component {
   render() {
     return (
+      <div>
       <p>Welcome, {this.props.name}!</p>
+      <p>My age is {this.props.age}</p>
+      </div>
     )
   }
 }
