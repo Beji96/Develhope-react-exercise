9fa62a755bc73c7a7e2f1f12dcc79818876b0a2a
Flavius Bejan <flaviusbejan96@yahoo.com>
2022-11-29 10:06:25 +0200

    finish forms 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-
 import './App.css';
-import ClickCounter from './ClickCounter';
-import ClickTracker from './ClickTracker';
-// import Counter from './Counter';
-import Hello from './Hello';
+import InteractiveWelcome from './InteractiveWelcome';
 import Welcome from './Welcome';
 
+
 function App() {
-  const myName = "John"
+  
  return(
   <div className="App">
-    <Hello/>
-    <Welcome name ={myName} age={26}/>
-   {/* <Counter/> */}
-   <ClickCounter/>
-   <ClickTracker/>
+   <InteractiveWelcome/>
   </div>
  )
 }
--- src/Message.js
+++
@@ -1,9 +0,0 @@
-import './App.css'
-
-function Message(){
-    return(
-        <p>What a beautiful day!</p>
-    )
-}
-
-export default Message
