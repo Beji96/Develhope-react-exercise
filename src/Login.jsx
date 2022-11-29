9bb8e36d1a9606baa10188eb6464759aacc65cfc
Flavius Bejan <flaviusbejan96@yahoo.com>
2022-11-29 11:07:29 +0200

    finish forms-3

--- src/Login.jsx
+++ src/Login.jsx
@@ -12,6 +12,7 @@ export default class Login extends Component {
         const name = event.target.name 
         const type = event.target.type 
         const checked = event.target.checked 
+        const submit = event.target.submit 
         this.setState({
             [name]:type  === 'checkbox'? checked: value,
             
@@ -19,41 +20,45 @@ export default class Login extends Component {
     }
    
   handleLoginState = ()=>{
-    this.setState({
-        username: '',
-        password: '',
-        remember: false,
-    })
+     this.setState({
+      logedIn: true,
+    });
+    
   }
 
   render() {
     return (
       <div>
-        <div>
-            <button onClick={this.handleLoginState}>Login</button>
-        </div>
-        <div>
-        <input 
-        type="text" 
-        name='username' 
-        placeholder='username'
-        value={this.state.username} 
-        onChange={this.handleInputChange} />
-        {/* password input */}
-        <input 
-        name='password'
-        type='password'
-        placeholder='password'
-        value={this.state.password}
-        onChange={this.handleInputChange}/>
-        {/* Checkbox */}
-        <input 
-         type="checkbox"
-         name='remember'
-         checked={this.state.remember}
-         onChange={this.handleInputChange} />
-        </div>
-        
+            <div>
+                <button 
+                 type="button"
+                 onClick={this.handleLoginState}
+                 disabled={!this.state.username || !this.state.password}
+                 >Login</button>
+               
+            </div>
+                <div>
+                    <input 
+                    type="text" 
+                    name='username' 
+                    placeholder='username'
+                    value={this.state.username} 
+                    onChange={this.handleInputChange} />
+                    {/* password input */}
+                    <input 
+                    name='password'
+                    type='password'
+                    placeholder='password'
+                    value={this.state.password}
+                    onChange={this.handleInputChange}/>
+                    {/* Checkbox */}
+                    <input 
+                    type="checkbox"
+                    name='remember'
+                    checked={this.state.remember}
+                    onChange={this.handleInputChange} />
+                </div>
+                
       </div>
     )
   }
