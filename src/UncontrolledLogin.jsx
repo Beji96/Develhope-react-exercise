67821392457a595ecdef7aac2a8eade024ca7e32
Flavius Bejan <flaviusbejan96@yahoo.com>
2022-11-30 11:34:52 +0200

    finish forms 6

--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 
 export default class UncontrolledLogin extends Component {
+  _formRef = createRef();
   handleFormSubmit = (event) => {
     event.preventDefault();
 
@@ -13,18 +14,26 @@ export default class UncontrolledLogin extends Component {
       remember,
     });
   };
+  handlePrefill = () => {
+    this._formRef.current.elements.username.value = "flavius96";
+    this._formRef.current.elements.password.value = "hello1212";
+    this._formRef.current.elements.remember.checked = true;
+  };
 
   render() {
     return (
       <div>
         UncontrolledLogin
-        <form onSubmit={this.handleFormSubmit}>
-          <input type="text" name="username" />
+        <form ref={this._formRef} onSubmit={this.handleFormSubmit}>
+          <input type="text" name="username" autoFocus />
           <input type="password" name="password" />
           <input type="checkbox" name="remember" />
 
           <button type="submit">Login</button>
           <button type="reset">Reset</button>
+          <button type="button" onClick={this.handlePrefill}>
+            Prefill
+          </button>
         </form>
       </div>
     );
