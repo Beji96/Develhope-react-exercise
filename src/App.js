560bd98e11a0f1c7d1159968d8d8e3d51e395a8d
Flavius Bejan <flaviusbejan96@yahoo.com>
2022-12-04 10:51:19 +0200

    finish use ref

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import useChangeEvent from "./useChangeEvent";
 import "./App.css";
+import { useRef, useState, useEffect } from "react";
+import CarDetails from "./CarDetails";
 
 function App() {
-  const { data, onChangeEvent } = useChangeEvent();
+  const [data, setData] = useState({ name: "", year: "", color: "" });
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.value = "";
+  }, []);
+
+  function inputChange(event) {
+    const { name, value } = event.target;
+    setData((data) => {
+      return {
+        ...data,
+        [name]: value,
+      };
+    });
+  }
 
   return (
     <div className="App">
-      <input
-        type="text"
-        name="username"
-        value={data.username}
-        onChange={onChangeEvent}
-      />
-      <input
-        type="password"
-        name="password"
-        value={data.password}
-        onChange={onChangeEvent}
-      />
-      <input
-        type="checkbox"
-        name="remember"
-        checked={data.remember}
-        onChange={onChangeEvent}
-      />
+      <form>
+        <label htmlFor="model">Model: </label>
+        <input ref={inputRef} type="text" name="model" onChange={inputChange} />
+        <label htmlFor="year"> Year: </label>
+        <input ref={inputRef} type="text" name="year" onChange={inputChange} />
+        <label htmlFor="color"> Color: </label>
+        <input ref={inputRef} type="text" name="color" onChange={inputChange} />
+        <button type="reset">Clear</button>
+      </form>
+      <CarDetails model={data.model} year={data.year} color={data.color} />
     </div>
   );
 }
