805226b60af8c82d77dbb9bd22a01d4a3fd55f8d
Flavius Bejan <flaviusbejan96@yahoo.com>
2022-12-04 10:59:23 +0200

    done useContext

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
 import "./App.css";
-import { useRef, useState, useEffect } from "react";
-import CarDetails from "./CarDetails";
+import DisplayLanguage from "./DisplayLanguage";
 
 function App() {
-  const [data, setData] = useState({ name: "", year: "", color: "" });
-
-  const inputRef = useRef();
-
-  useEffect(() => {
-    inputRef.current.value = "";
-  }, []);
-
-  function inputChange(event) {
-    const { name, value } = event.target;
-    setData((data) => {
-      return {
-        ...data,
-        [name]: value,
-      };
-    });
-  }
-
   return (
     <div className="App">
-      <form>
-        <label htmlFor="model">Model: </label>
-        <input ref={inputRef} type="text" name="model" onChange={inputChange} />
-        <label htmlFor="year"> Year: </label>
-        <input ref={inputRef} type="text" name="year" onChange={inputChange} />
-        <label htmlFor="color"> Color: </label>
-        <input ref={inputRef} type="text" name="color" onChange={inputChange} />
-        <button type="reset">Clear</button>
-      </form>
-      <CarDetails model={data.model} year={data.year} color={data.color} />
+      <DisplayLanguage />
     </div>
   );
 }
