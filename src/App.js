52ce2f349b62b4511c28458eff9b1127374d71d9
Flavius Bejan <flaviusbejan96@yahoo.com>
2022-12-04 11:03:32 +0200

    done calll backs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import DisplayLanguage from "./DisplayLanguage";
-
+import useCounter from "./useCounter";
 function App() {
+  const { counter, onIncrement, onDecrement, onReset } = useCounter();
+
   return (
     <div className="App">
-      <DisplayLanguage />
+      <h1>{counter}</h1>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+      <button onClick={onReset}>Reset</button>
     </div>
   );
 }
